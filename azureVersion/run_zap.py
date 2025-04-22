0603ebd7cbbbc6a7a6c64aee3211e714e88d89f5
philprotonphil <philprotonphil@protonmail.com>
2025-04-22 17:54:38 +0100

    working zap

---
+++ azureVersion/run_zap.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+
+command = [
+    "docker", "run",
+    "-v", f"{os.getcwd()}:/zap/wrk/:rw",
+    "-t", "zaproxy/zap-stable",
+    "zap.sh", "-cmd", "-autorun", "/zap/wrk/updated_zap.yaml"
+]
+
+try:
+    result = subprocess.run(command, text=True, capture_output=True)
+    print(result.stdout)
+    print("Command executed successfully!")
+except subprocess.CalledProcessError as e:
+    print("Error occurred while running the command:")
+    print(e.stderr)
