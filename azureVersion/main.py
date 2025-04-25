43a4e1b1b9a8657d32c1c699da8db1dfd62572e0
philprotonphil <philprotonphil@protonmail.com>
2025-04-25 09:50:41 +0100

    working

--- azureVersion/main.py
+++ azureVersion/main.py
@@ -1,10 +1,9 @@
-import frontend
-import frontend.frontend_ai
+from frontend import frontend_ai
 from middleware import run_zap
 from backend import backend_ai
 from backend import XML_to_db
 
-# frontend.frontend_ai.frontend()
-# run_zap.startzap()
-# XML_to_db.xml_to_db()
+frontend_ai.frontend()
+run_zap.startzap()
+XML_to_db.xml_to_db()
 backend_ai.query_and_graph()
